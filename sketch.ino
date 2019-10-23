9d124243a663b78fdfadf3f76a9cdd30d29291cc
Brandon Tiqui <btiqui@gmail.com>
2019-10-22 21:13:46 -0700

    Add second controller

--- sketch.ino
+++ sketch.ino
@@ -1,28 +1,52 @@
 #include <NintendoExtensionCtrl.h>
 
 Nunchuk leftNchuk(Wire);
+Nunchuk rightNchuk(Wire1);
 
 void setup() {
   Serial.println("test");
   // I2C Init
   leftNchuk.begin();
+  rightNchuk.begin();
   leftNchuk.i2c().setClock(400000);
+  rightNchuk.i2c().setClock(400000);
 
   boolean leftConnected = false;
+  boolean rightConnected = false;
 
   // connect left controller
   while (!(leftConnected)) {
     Serial.println("left is not connected");
-    if (!leftConnected)  { leftConnected  = leftNchuk.connect(); }
+    leftConnected  = leftNchuk.connect();
   }
   Serial.println("left is connected");
+  // connect right controller
+  while (!(rightConnected)) {
+    Serial.println("right is not connected");
+    rightConnected = rightNchuk.connect();
+  }
+  Serial.println("right is connected");
 }
 
 void loop() {
-  if (leftNchuk.buttonC()) {
-    Serial.println("left c pressed");
+  boolean leftReady = leftNchuk.update();
+  boolean rightReady = rightNchuk.update();
+
+  if (leftReady) {
+    if (leftNchuk.buttonC()) {
+      Serial.println("left c pressed");
+    }
+    if (leftNchuk.buttonZ()) {
+      Serial.println("left z pressed");
+    }
   }
-  if (leftNchuk.buttonZ()) {
-    Serial.println("left z pressed");
+
+  if (rightReady) {
+    if (rightNchuk.buttonC()) {
+      Serial.println("right c pressed");
+    }
+    if (rightNchuk.buttonZ()) {
+      Serial.println("right z pressed");
+    }
   }
 }
